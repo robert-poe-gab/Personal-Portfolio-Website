91bfc1fd676180b6711c945107032a9d0dbdc1f3
Robert Gabriel <robert.pooenaru@gmail.com>
2025-11-20 16:23:16 +0100

    php->html

--- public/index.php
+++ public/index.php
@@ -1,35 +1,43 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Robert | Portfolio</title>
 
-// --------- Emeset --------- 
-include "../src/Emeset/Container.php";
-include "../src/Emeset/Request.php";
-include "../src/Emeset/Response.php";
-include "../src/Container.php";
+    <!-- CSS -->
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/infiniteMenu.css">
 
-// --------- Controllers ---------
-include "../src/controllers/ctrlMain.php";
+        <!-- JS -->
+        <script src="/js/bootstrap.min.js" defer></script>
+        <!-- Built bundle for InfiniteMenu (produced by Vite build)
+                After running `npm install` and `npm run build`, a bundle
+                will appear at `/dist/infiniteMenu.iife.js` (or similar).
+                Include it below so the site can mount the component.
+        -->
+        <script src="/dist/infiniteMenu.iife.js" defer></script>
+</head>
+<body>
 
-// --------- Config ---------
-$config = [];
+<h1>Welcome to My Portfolio</h1>
 
-$container = new \Container($config);
-$request = $container->request();
-$response = $container->response();
+<div id="infinite-menu-container" style="height: 600px; position: relative;"></div>
 
-// --------- Routes ---------
-$r = '';
-if(isset($_REQUEST["r"])){
-    $r = $_REQUEST["r"];
-}
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    const items = [
+        { image: 'https://picsum.photos/300/300?grayscale', link: '#', title: 'Item 1', description: 'This is pretty cool, right?' },
+        { image: 'https://picsum.photos/400/400?grayscale', link: '#', title: 'Item 2', description: 'This is pretty cool, right?' },
+        { image: 'https://picsum.photos/500/500?grayscale', link: '#', title: 'Item 3', description: 'This is pretty cool, right?' },
+        { image: 'https://picsum.photos/600/600?grayscale', link: '#', title: 'Item 4', description: 'This is pretty cool, right?' }
+    ];
 
-if($r === "") {
-    $response = ctrlMain($request, $response, $container);
-}
-elseif($r === "index") {
-    $response = ctrlIndex($request, $response, $container);
-}
-else {
-    $response->ctrl404($request, $response, $container);
-}
+    // Expose items for the frontend bundle to consume.
+    window.INFINITE_MENU_ITEMS = items;
+});
+</script>
 
-$response->response();
+</body>
+</html>
--- src/controllers/ctrlMain.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function ctrlMain($request, $response, $container){
-    $response->setTemplate("main.php");
-
-    return $response;
-    
-}
